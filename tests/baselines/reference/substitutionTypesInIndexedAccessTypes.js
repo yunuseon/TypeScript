30f15ab066afaf80107f13e8c27f31dee1a6487b
Yunus Öztürk <yunusozturk@outlook.de>
2019-06-19 00:06:20 +0200

    Update substitutionTypesInIndexedAccessTypes baseline

--- tests/baselines/reference/substitutionTypesInIndexedAccessTypes.js
+++ tests/baselines/reference/substitutionTypesInIndexedAccessTypes.js
@@ -5,8 +5,6 @@ type UserArgs = {
   select?: boolean
 };
 
-type Subset<T, U> = { [key in keyof T]: key extends keyof U ? T[key] : never };
-
 declare function withBoundary<T extends UserArgs>(args?: Subset<T, UserArgs>): T;
 declare function withoutBoundary<T extends UserArgs>(args?: T): T;
 
